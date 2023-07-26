987c7a1080cecc80891bc7801456c96d322c8d0c
ngud-119 <passiondev7@gmail.com>
2023-07-26 16:01:55 +0600

    ready-project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1>regregrg</h1>
+  </main>
+</template>
